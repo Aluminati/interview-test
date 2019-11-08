82be6fe6f8e1c75480a2ac3505da3f34c137e35a
EbiO22 <53309645+EbiO22@users.noreply.github.com>
2019-11-08 17:31:35 +0000

    Update Numbers.vue
    Line 32- changed output from 0.5 to 1 because I want the code to execute 100% of the time.
    Line 44- changed divisor to 100, and number to 12

--- src/components/Numbers.vue
+++ src/components/Numbers.vue
@@ -29,7 +29,7 @@ export default {
       {
         numbers = [...numbers, i];
       }
-      return numbers.sort(() => Math.random() - 0.5);
+      return numbers.sort(() => Math.random() - 1);
     },
     hov(number)
     {
@@ -41,7 +41,7 @@ export default {
         if(number % num === 0)
         {
           nums[i].classList.add('active')
-          console.log('divisor', num)
+          console.log('100', 12)
         }
       }
     },
